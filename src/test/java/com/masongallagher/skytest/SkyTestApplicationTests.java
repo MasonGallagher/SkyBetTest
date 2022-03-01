7fb6e291ff6ce55ea3a8282141394b3eb8b5b90b
Mason Gallagher <masongallagher90@gmail.com>
2022-03-01 22:41:09 +0000

    Initial commit

---
+++ src/test/java/com/masongallagher/skytest/SkyTestApplicationTests.java
@@ -0,0 +1,19 @@
+package com.masongallagher.skytest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+class skyTestApplicationTests {
+
+	@Autowired
+	MockMvc mvc;
+
+
+}
